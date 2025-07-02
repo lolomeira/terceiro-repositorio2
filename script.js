40271abaa11dcffe25ea48b5ed48ed545975e943
lolomeira <138534956+lolomeira@users.noreply.github.com>
2025-07-01 21:28:43 -0300

    Aula 4 - 2

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function diminuiTamanho(){
         tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 
 function aumentaTamanho(){
@@ -21,4 +22,44 @@ function aumentaTamanho(){
         tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+
+const campoSenha = document.querySelector("#campo-senha");
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+
+const chekbox = document.querySelectorAll('.chekbox');
+
+function gerasenha() {
+    let alfabeto = '';
+        if (chekbox[0].cheked)  {
+            alfabeto =alfabeto + letrasMinusculas;
+        }
+         if (chekbox[1].cheked)  {
+            alfabeto =alfabeto + letrasMaiusculas;
+        }
+         if (chekbox[2].cheked)  {
+            alfabeto =alfabeto + numeros;
+        }
+         if (chekbox[3].cheked)  {
+            alfabeto =alfabeto + simbolos;
+        }
+        console.log(alfabeto);
+        let senha = '';
+        for (let i=0; i < tamanhoSenha; i++) {
+            let numeroAleatorio = math.random()*alfabeto.length;
+            numeroAleatorio = math.floor(numeroAleatorio);
+            senha = senha + alfabeto[numeroAleatorio];
+        }
+        campoSenha.value = senha;
+}
+
+gerasenha();
+
+for (i=0; i< chekbox.length; i++) {
+    chekbox[i].onclik = geraSenha;
 }
